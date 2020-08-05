cc71e8974bc759ea6bc52cca4ebda5c24410baaa
Vitor Teixeira <vitor.teixeira@bigbrain.com.br>
2020-08-05 15:51:28 -0300

    Criação de exemplo de resolver genérico em Create User

--- src/Resolvers.ts
+++
@@ -1,9 +0,0 @@
-import { RegisterResolver } from "./modules/user/Register"
-import { LoginResolver } from "./modules/Login";
-import { ConfirmUserResolver } from "./modules/user/ConfirmUser";
-
-export const Resolvers = [        
-    RegisterResolver, 
-    LoginResolver,
-    ConfirmUserResolver
-]
